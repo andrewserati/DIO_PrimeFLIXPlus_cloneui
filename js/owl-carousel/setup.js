1b18b187a5187c0dc44c930770417fd7f0c44f87
SirLordX <andrewse2015@hotmail.com>
2020-12-31 00:52:32 -0300

    initial commit

---
+++ js/owl-carousel/setup.js
@@ -0,0 +1,31 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: false,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2.5
+        },
+        500: {
+            items: 3.5
+        },
+        700:{
+            items:4.5
+        },
+        800: {
+            items: 5.5
+        },
+        1000:{
+            items:5.5
+        },
+        1100: {
+            items: 6.5
+        },
+        1300: {
+            items: 7.5
+        }
+    }
+})
